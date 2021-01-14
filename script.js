f56b65069fd4154c988c9d05939761a44970e389
Simone Cardinale <simonecardinale8@gmail.com>
2021-01-14 16:03:19 +0100

    comments created

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-$(document).ready(function () {
-    
-    // definisco l'array e inserisco 5 valori casuali
+
+    // definisco l'array e inserisco 5 valori casuali senza ripetizioni
 
     var array = [];
+    var arrayUtente = [];
+    var arrayRisultato =[];
+    
+    var j = 0
+    
+    while (j < 5) {
+        var numeri_array = Math.floor(Math.random()*(100 - 1 + 1) + 1);
+        if (array.includes(numeri_array) == false){
+            array.push(numeri_array);
+        }
+        j++;
+    };
+    
+    console.log(array);
 
-for (var i = 0; i < 5 ; i++){
-    var numeri_array = Math.floor(Math.random()*(100 - 1 + 1) + 1);
-    array.push(numeri_array);
-};
-
-// inserisco l'alert
-
-
-document.getElementById("array_container_text").innerHTML = array;
-var alertClock = setTimeout(alertTime, 3000);
-var promptClock = setTimeout(promptUtente, 3000);
-
-
-
-
-
-
+// inserisco l'alert e definisco l'inserimento nel secondo array
 
 
-
-//funzioni
-
-function alertTime(){
-    $(".array_container").addClass('none');
-}
-
-function promptUtente(){
+alert(array);
+var promptClock = setTimeout(function(){
     for (i = 0; i < 5; i++){
         var utente = parseInt(prompt("Inserisci un numero"));
-    }
-}
+        arrayUtente.push(utente);
 
-});
+        // descrivo le condizioni di vittoria
+
+        if (array.includes(arrayUtente[i]) && arrayUtente[i]!= arrayUtente[i]) {
+            arrayRisultato.push(arrayUtente[i]);
+        }
+    }
+    console.log(arrayRisultato);
+}, 3000);
+
+
+
+
+
+
+
+
+
